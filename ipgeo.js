14b2d9e7d5589929b18655827403258ef7768ad3
wildy1050 <121340071+wildy1050@users.noreply.github.com>
2022-12-25 10:25:54 +0700

    Use with JavaScript

--- ipgeo.js
+++ ipgeo.js
@@ -11,7 +11,7 @@ Language  : JavaScript
 
 This code is used to retrieve network information from visitors
 and is released under the "Mozilla Public License Version 2.0".
-Please see < https://raw.githubusercontent.com/wildy1050/ipgeo/main/LICENSE >
+Please see < https://raw.githubusercontent.com/wildy1050/license/main/mpl-v2 >
 */
 
 fetch(`https://api.wildy.my.id/ipgeo/`, {
